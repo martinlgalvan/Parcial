b770bfbb91c8dcaf5e83db37c3403c6be95d4c1d
martinlgalvan <martinlgalvan@yahoo.com>
2022-06-22 22:14:16 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-hjr4fy
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-hjr4fy)
---
+++ src/components/Logo.vue
@@ -0,0 +1,15 @@
+<template>
+  <img src="https://vuejs.org/images/logo.png" alt="" />
+</template>
+
+<script>
+export default {
+  name: 'Logo',
+};
+</script>
+
+<style scoped>
+img {
+  width: 50px;
+}
+</style>
